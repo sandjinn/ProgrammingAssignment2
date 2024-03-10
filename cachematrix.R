3b7a2bcfe394b4e3313c8a1e3b5fcce2e3a665ab
sandjinn <annalize.rheeder@gmail.com>
2024-03-10 10:06:51 +0100

    Edited original file according to assignment instructions. Added code for two functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Coursera Data Specialisation
+### R programming
+### Assignment 2
+### Sandjinn
 
-## Write a short comment describing this function
+## This code creates two functions in order to save the a solved matrix in the cache
+## And to retrieve it from the cache.
 
-makeCacheMatrix <- function(x = matrix()) {
+## makeCacheMatrix
+## This function creates a special "matrix" object that can cache its inverse.
+## The matrix is contained inside a list as one of the object
 
+makeCacheMatrix <- function(m_og = matrix()) {
+    m_sol <- NULL
+    set <- function(y) {
+        m_og <<- y
+        m_sol <<- NULL
+    }
+    get <- function() m_og
+    setsolve <- function(solve) m_sol <<- solve
+    getsolve <- function() m_sol
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m_sol <- x$getsolve()
+    if(!is.null(m_sol)) {
+        message("getting cached data")
+        return(m_sol)
+    }
+    data <- x$get()
+    m_sol <- solve(data, ...)
+    x$setsolve(m_sol)
+    m_sol
 }
+
+## Testing the functions
+
+# create a matrix
+mat <- matrix(rnorm(1:9), 3, 3)
+
+# create the cachelist using the above matrix
+mat_cache <- makeCacheMatrix(mat)
+
+# retrieve the solve value from the cache, or calculate if not existing
+cacheSolve(mat_cache)
+
+# double check result by calculating the solve seperatly
+mat_inv <- solve(mat)
+mat_inv
